75235ddf0b9095fc84018110ce997f895de78d8a
sm-mehedi <hasanmehedi63395@gmail,com>
2025-05-25 20:39:48 +0600

    Created species Table

--- conservation_db.sql
+++ conservation_db.sql
@@ -6,3 +6,10 @@ CREATE TABLE rangers (
     region VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(20) NOT NULL
+);
