262bcda6d98ef2456b0cbdab0f91722298277d1d
sm-mehedi <hasanmehedi63395@gmail,com>
2025-05-25 20:33:56 +0600

    created rangers table

--- conservation_db.sql
+++ conservation_db.sql
@@ -1,1 +1,8 @@
 -- Active: 1747980861147@@127.0.0.1@5432@conservation_db
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
+
