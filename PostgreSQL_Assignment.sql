8df55f1bd79affe562350b186b797f1d1a0c6362
sm-mehedi <hasanmehedi63395@gmail,com>
2025-05-26 14:23:40 +0600

    ALTERTED data type of notes column in sightings table to TEXT

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -39,6 +39,10 @@ CREATE TABLE sightings (
     notes VARCHAR(100)
 );
 
+ALTER TABLE sightings
+ALTER COLUMN notes TYPE TEXT;
+
+
 insert into sightings (species_id, ranger_id, location, sighting_time, notes)
 Values(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
       (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
@@ -78,7 +82,6 @@ SELECT sighting_id, time_of_day
 FROM sightings;
 
 -- Problem 9
-
 DELETE FROM rangers
 WHERE ranger_id NOT IN (
     SELECT DISTINCT ranger_id FROM sightings
